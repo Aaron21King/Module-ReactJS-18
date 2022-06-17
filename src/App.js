2bf9c67566fccd50ca94cbed86a78aba372dede5
Aaron21King <lovespurs1882@gmail.com>
2022-06-16 18:50:58 -0600

    subiendo clase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
+
 import "./App.css";
 
-//Components
+// Components
 import Title from "./components/Title";
 import FullName from "./components/FullName";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log(count, "COUNT");
-  console.log(setCount, "SET_COUNT");
+  const [isOn, setIsOn] = useState(false);
 
   const handleClick = (increase) => {
     let newCount = count;
@@ -20,14 +20,30 @@ function App() {
     setCount(newCount);
   };
 
+  const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
+  const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
+  const countClass = isMultiple ? "active" : "inactivo";
+
+  const squareClass = isOn ? "square-on" : "square-off";
+  const squareText = isOn ? "ON" : "OFF";
   return (
     <div className="App">
       <header className="App-header">
         <Title text="Aaron"></Title>
-        <FullName firstName="Aaron Armando" lastName="Enciso Guzman"></FullName>
-        <p>Counter: {count}</p>
+        <FullName firstName="Aaron" lastName="Enciso"></FullName>
+        <p className={countClass}>
+          Counter: {count}, {countType}
+        </p>
         <button onClick={() => handleClick(true)}>Increase</button>
-        <button onClick={() => handleClick(false)}>Decrease</button>
+
+        <div className={`square ${squareClass}`}>{squareText}</div>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <button onClick={() => setIsOn(!isOn)}>Interruptor</button>
+        </div>
       </header>
     </div>
   );
