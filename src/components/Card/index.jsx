31d879a4916431ebdb729eb174e2982d92369940
Aaron21King <lovespurs1882@gmail.com>
2022-06-21 18:31:30 -0600

    clase6

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,22 @@
-import CardIC from "../CardItemContent";
+// Component
+import CardItemContent from "../CardItemContent";
+
 import "./Card.css";
 
-export default function Card({
-  photoURL,
-  
-}) {
+export default function Card({ photoURL, firstName, lastName, age, gender }) {
   return (
     <div className="card-container">
       <div className="card-image-container">
         <img alt="profile" src={photoURL} className="card-image" />
       </div>
       <div className="card-content">
-        <CardIC className="card-item-content" ></CardIC>
-
+        {/* Crear un componente llamado CardItemContent */}
+        <CardItemContent title={"Name"} content={`${firstName} ${lastName}`} />
+        <CardItemContent title={"Edad"} content={age} />
+        <CardItemContent
+          title={"Gender"}
+          content={gender === "m" ? "Masculino" : "Femenino"}
+        />
       </div>
     </div>
   );
--- src/components/CardItemContent/index.jsx
+++ src/components/CardItemContent/index.jsx
@@ -1,26 +1,10 @@
 import "./CardIC.css";
 
-export default function CardIC({
-    firstName,
-    lastName,
-    age,
-    gender,
-  }) {
-    return (
-        <div className="card-item-content">
-            <p className="title"></p>
-            <p className="content">
-                {firstName} {lastName}
-            </p>
-        <div className="card-item-content">
-          <p className="title">Edad</p>
-          <p className="content">{age}</p>
-        </div>
-        <div className="card-item-content">
-          <p className="title">Gender</p>
-          <p className="content">{gender === "m" ? "Masculino" : "Femenino"}</p>
-        </div>
-        </div>
-      
-    );
-  }
+export default function CardItemContent({ title, content }) {
+  return (
+    <div className="card-item-content">
+      <p className="title">{title}</p>
+      <p className="content">{content}</p>
+    </div>
+  );
+}
