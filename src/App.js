1b762e5694740d8d2fa8783c0cd1a545fff28b9e
Aaron21King <lovespurs1882@gmail.com>
2022-06-23 19:20:40 -0600

    clase09

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+
+// Containers
 import AppContainer from "./containers/AppContainer";
+import ProfileContainer from "./containers/ProfileContainer";
+
+// Components
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Editar from "./pages/Profile/editP";
+import ProfileDetail from "./pages/Profile/Detail";
+import ProfileEdit from "./pages/Profile/Edit";
 
 function App() {
   return (
@@ -16,8 +21,10 @@ function App() {
         <Route path="app" element={<AppContainer />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="edit" element={<Editar/>} />
+          <Route path="profile" element={<ProfileContainer />}>
+            <Route index element={<ProfileDetail />} />
+            <Route path="edit" element={<ProfileEdit />} />
+          </Route>
         </Route>
       </Routes>
     </div>
