274cb98902968f027cebe576fabd24a15d8e88a4
Aaron21King <lovespurs1882@gmail.com>
2022-06-29 21:01:37 -0600

    clase13

---
+++ src/pages/Profile/Create/index.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { create as createUser } from "../../../services/users";
+
+//componets
+import Inputs from "../../../components/Inputs";
+
+// CSS
+import "./Create.css";
+
+export default function ProfileCreate() {
+  // Local state
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+
+  const cleanForm = () => {
+    setFirstName("");
+    setLastName("");
+    setPhotoURL("");
+    setEmail("");
+    setBirthdate("");
+  };
+
+  const isEmpty = (value) => !value;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (
+      isEmpty(firstName) ||
+      isEmpty(lastName) ||
+      isEmpty(photoURL) ||
+      isEmpty(email) ||
+      isEmpty(birthdate)
+    ) {
+      return;
+    }
+
+    const data = {
+      firstName,
+      lastName,
+      photoURL,
+      email,
+      birthdate,
+    };
+
+    try {
+      await createUser(data);
+      cleanForm();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Create</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-container">
+          <Inputs
+            placeholder="First Name"
+            value={firstName}
+            callback={(e) => setFirstName(e.target.value)}
+          />
+          <Inputs
+            placeholder="Last Name"
+            value={lastName}
+            callback={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <Inputs
+          placeholder="Email "
+          value={email}
+          callback={(e) => setEmail(e.target.value)}
+        />
+
+        <Inputs
+          placeholder="Birthdate"
+          value={birthdate}
+          callback={(e) => setBirthdate(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Create User
+        </button>
+      </form>
+    </div>
+  );
+}
+Footer
--- src/pages/Profile/Detail/index.jsx
+++ src/pages/Profile/Detail/index.jsx
@@ -1,3 +1,55 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { retrieve as retrieveUser } from "../../../services/users";
+import { deleteList as deleteUsers } from "../../../services/users";
+
+import "./Detail.css";
+
 export default function ProfileDetail() {
-  return <h2>Detail</h2>;
+
+  const [user, setUser] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await retrieveUser(id);
+     
+      setTimeout(() => {
+        setUser(data);
+      }, 1000);
+      console.log(data);
+    };
+
+    getUser();
+  }, []);
+
+
+
+  return (
+    <div>
+      <h2>Detail</h2>
+      {!user ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <img src={user.photoURL} />
+          <p>
+            {user.firstName} {user.lastName}
+          </p>
+          <p>{user.email}</p>
+          <button onClick={() => navigate(-1)}>Regresar</button>
+          <button onClick={() => navigate(1)}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
 }
+
+// EJERCICIO: EDIT PROFILE
+// 1. Recibir variable de la URL
+// 2. Consultar la información en la DB (firebase)
+// 3. Cuando se tenga disponible la información,
+// ACTUALIZAR EL ESTADO DE LOS INPUTS
+// 4. Crear formulario
+// 5. Enviar PATCH a firebase
